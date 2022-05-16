f8eff64cbeb7dc91acb9b020f93d21ba6c4ae535
Ajay <ajaychoudhary19990@gmail.com>
2022-05-16 08:42:36 +0530

    Added validation to get tasks api query params

--- src/tasks/dto/get-task-filter.dto.ts
+++ src/tasks/dto/get-task-filter.dto.ts
@@ -1,6 +1,12 @@
-import { TaskStatus } from "../task.model";
+import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
+import { TaskStatus } from '../task.model';
 
 export class GetTaskFilterDto {
+  @IsOptional()
+  @IsNotEmpty()
   search?: string;
+
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
 }
